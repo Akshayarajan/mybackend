efae364d995806d72e06d1b2b343e2697748f42c
Akshayarajan <43717725+Akshayarajan@users.noreply.github.com>
2018-11-30 13:08:44 +0530

    Add files via upload

---
+++ src/main/java/com/ecomm/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.ecomm.dao;
+
+import com.ecomm.model.Customer;
+
+public interface CustomerDAO {
+	
+		void registerCustomer(Customer customer);
+		boolean isEmailUnique(String email);
+
+
+	}	
